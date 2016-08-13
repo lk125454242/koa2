2d2c47058c39f41fb4a1908ca33f3209bd1ee4ad
lk125454242 <125454242@qq.com>
2016-08-13 14:37:19 +0800

    不知为何 先404 然后 才返回数据

--- routes/users.js
+++ routes/users.js
@@ -2,36 +2,38 @@ var router = require('koa-router')();
 var users_mongoose = require('../vilidate/users_mongoose');
 /* 注册账户 */
 router.post('/register', async (ctx, next) => {
-    var user,username,password1,password2,data,param = ctx.request.body;
+    var user,username,password1,password2,data,param = ctx.request.body,result;
     username = param.username;
     password1 = param.password[0];
     password2 = param.password[1];
     await users_mongoose.find_user(username).then(function (data) {
-        user = data;
-    }).catch(function (err) {
-        users_mongoose.error(ctx);
-    });
-    console.log(user);
-    if(user){
-        ctx.body = {
-            code: 401,
-            msg: '已经被注册，请修改账户名'
+        console.log(1);
+        if(data){
+            throw new Error('已经被注册，请修改账户名');
         }
-    }else {
-        if(password1 === password2){
-            data = await users_mongoose.save_user(username,password1);
-            ctx.body = {
+    }).then(function () {
+        console.log(2);
+        if(password1 !== password2){
+            throw new Error('两次密码不一致');
+        }
+    }).then( async function () {
+        console.log(3);
+        ctx.body = await users_mongoose.save_user(username,password1).then(function (data) {
+            return {
                 code: 200,
                 data: data,
                 msg: '注册成功'
-            }
-        }else {
-            ctx.body = {
-                code: 401,
-                msg: '两次密码输入不一致'
-            }
-        }
-    }
+            };
+        });
+    }).catch(function (err) {
+        console.log(4);
+        ctx.body = {
+            code: 401,
+            msg: err
+        };
+    }).done(function (err) {
+        console.log(err);
+    });
 });
 router.get('/get_users', async (ctx, next) => {
     var data;
--- vilidate/users_mongoose.js
+++ vilidate/users_mongoose.js
@@ -4,45 +4,28 @@
 var module = require('../database/mongodb/all_module');
 var User = module.User;
 
-exports.error = function (ctx) {
-    ctx.body = {
-        code: 500,
-        msg:'服务器错误'
-    }
-};
-
 exports.find_user = function (username) {
-    return new Promise( (resolve,reject) => {
-        User.find({username: username}, function (err, users) {
-            if (err) {
-                reject('find_user',err);
-            } else {
-                resolve(users);
-            }
-        });
-    })
+    return User.find({username: username}).then(function (data) {
+        return data;
+    },function (err) {
+        throw new Error('find_user:服务器错误');
+    });
 };
 exports.save_user = function (username, password) {
     var newUser = new User({
         username: username,
         password: password
     });
-    newUser.save(function (err, newUser) {
-        if (err) {
-            console.log('save_user',err);
-            return false;
-        } else {
-            return newUser;
-        }
+    return newUser.save().then(function (data) {
+        return data;
+    },function (err) {
+        throw new Error('save_user 服务器错误');
     })
 };
 exports.get_users = function () {
-    User.find({},function (err, users) {
-        if(err){
-            console.log('get_users',err);
-            return false;
-        }else {
-            return users;
-        }
+    return User.find({},function (data) {
+        return data;
+    },function (err) {
+        throw new Error('get_users 服务器错误');
     });
 };
